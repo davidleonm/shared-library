b26e618bbb443b8bc534350bfd0bbdc2b1be787d
David <david.leon@nexthink.com>
2020-03-16 15:32:22 +0100

    Fixing env variables

--- src/com/davidleonm/PythonHelloWorldVariables.groovy
+++ src/com/davidleonm/PythonHelloWorldVariables.groovy
@@ -7,7 +7,7 @@ class PythonHelloWorldVariables {
   static String DockerHubRegistryName = 'davidleonm/pythonhelloworld'
 
   // The Jenkins server is the host of the environment so the URL for CI request is the same but different port
-  static String ContinuousIntegrationUrl = "${env.JENKINS_URL}".replace('8080/', '9999')
+  static String ContinuousIntegrationUrl = "${this.env.JENKINS_URL}".replace('8080/', '9999')
   static String ContinuousIntegrationForbiddenAnswer = 'Forbidden site!'
   static String ContinuousIntegrationAllowedAnswer = 'Hello World!'
 }
--- vars/executePythonHelloWorldCITest.groovy
+++ vars/executePythonHelloWorldCITest.groovy
@@ -2,10 +2,10 @@ import com.davidleonm.PythonHelloWorldVariables
 
 def call() {
   def dockerImage = null
-  def ciImageName = "python-hello-world-ci-image-${BUILD_ID}"
+  def ciImageName = "python-hello-world-ci-image-${this.env.BUILD_ID}"
 
   try {
-    dockerImage = docker.build("${ciImageName}", "--file ./Dockerfile ${WORKSPACE}")
+    dockerImage = docker.build("${ciImageName}", "--file ./Dockerfile ${this.env.WORKSPACE}")
 
     dockerImage.withRun("-p 9999:9999 --name=${ciImageName} -d") {
       forbiddenResponse = sh(script: "curl ${PythonHelloWorldVariables.ContinuousIntegrationUrl}", returnStdout: true)
