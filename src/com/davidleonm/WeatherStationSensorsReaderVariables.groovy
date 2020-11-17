82370342d94fe681ee6f0873741eeeaccf4cd0f8
David <david.leon@nexthink.com>
2020-11-18 00:44:27 +0100

    adding variables for reader

---
+++ src/com/davidleonm/WeatherStationSensorsReaderVariables.groovy
@@ -0,0 +1,8 @@
+package com.davidleonm
+
+import com.davidleonm.GlobalVariables
+
+class WeatherStationSensorsReaderVariables {
+  static String RepositoryName = 'weather-station-sensors-reader'
+  static String DockerHubRegistryName = 'davidleonm/weather-station-sensors-reader'
+}
