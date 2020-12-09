0cbf5a8a026a56976c00e7ff49e87649c943c5f1
David León <david.leon@nexthink.com>
2020-12-09 11:02:11 +0100

    new variable

--- src/com/davidleonm/WeatherStationSensorsReaderVariables.groovy
+++ src/com/davidleonm/WeatherStationSensorsReaderVariables.groovy
@@ -3,6 +3,7 @@ package com.davidleonm
 import com.davidleonm.GlobalVariables
 
 class WeatherStationSensorsReaderVariables {
+  static String CoverallsRepoToken = credentials('coveralls-sensors-reader-repo-token')
   static String RepositoryName = 'weather-station-sensors-reader'
   static String DockerHubRegistryName = 'davidleonm/weather-station-sensors-reader'
   static String DockerHubStagingRegistryName = 'davidleonm/staging'
