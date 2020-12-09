a5205de10bb6e6d82b4a63be29b256499420cc1f
David León <david.leon@nexthink.com>
2020-12-09 11:14:22 +0100

    Fixing variable

--- src/com/davidleonm/WeatherStationSensorsReaderVariables.groovy
+++ src/com/davidleonm/WeatherStationSensorsReaderVariables.groovy
@@ -3,7 +3,7 @@ package com.davidleonm
 import com.davidleonm.GlobalVariables
 
 class WeatherStationSensorsReaderVariables {
-  static String CoverallsRepoToken = credentials('coveralls-sensors-reader-repo-token')
+  static CoverallsRepoToken = credentials('coveralls-sensors-reader-repo-token')
   static String RepositoryName = 'weather-station-sensors-reader'
   static String DockerHubRegistryName = 'davidleonm/weather-station-sensors-reader'
   static String DockerHubStagingRegistryName = 'davidleonm/staging'
