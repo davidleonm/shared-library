3d726c65919b390687155a4578f4a636eb39fcd7
David León <david.leon@nexthink.com>
2020-12-09 11:17:13 +0100

    Removing variable

--- src/com/davidleonm/WeatherStationSensorsReaderVariables.groovy
+++ src/com/davidleonm/WeatherStationSensorsReaderVariables.groovy
@@ -3,7 +3,6 @@ package com.davidleonm
 import com.davidleonm.GlobalVariables
 
 class WeatherStationSensorsReaderVariables {
-  static CoverallsRepoToken = credentials('coveralls-sensors-reader-repo-token')
   static String RepositoryName = 'weather-station-sensors-reader'
   static String DockerHubRegistryName = 'davidleonm/weather-station-sensors-reader'
   static String DockerHubStagingRegistryName = 'davidleonm/staging'
