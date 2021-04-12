fc846164a0e87b99cf415bbea95ca9ad52e5423a
David <33702675+davidleonm@users.noreply.github.com>
2021-04-12 12:50:23 +0200

    Update WeatherStationSensorsReaderVariables.groovy

--- src/com/davidleonm/WeatherStationSensorsReaderVariables.groovy
+++ src/com/davidleonm/WeatherStationSensorsReaderVariables.groovy
@@ -3,7 +3,7 @@ package com.davidleonm
 import com.davidleonm.GlobalVariables
 
 class WeatherStationSensorsReaderVariables {
-  static String RepositoryName = 'weather-station-sensors-reader'
+  static String RepositoryName = 'weather-station-project/sensors-reader'
   static String DockerHubRegistryName = 'weatherstationproject/sensors-reader'
   static String DockerHubStagingRegistryName = 'davidleonm/staging'
 }
