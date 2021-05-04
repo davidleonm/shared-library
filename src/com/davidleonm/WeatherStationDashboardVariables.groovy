f2e19e630742c2d8fab66b8e4a8ff902c808b553
David Leon <david.leon@nexthink.com>
2021-05-04 14:53:50 +0200

    dashboard variables

---
+++ src/com/davidleonm/WeatherStationDashboardVariables.groovy
@@ -0,0 +1,9 @@
+package com.davidleonm
+
+import com.davidleonm.GlobalVariables
+
+class WeatherStationDashboardVariables {
+  static String RepositoryName = 'weather-station-project/dashboard'
+  static String DockerHubRegistryName = 'weatherstationproject/dashboard'
+  static String DockerHubStagingRegistryName = 'davidleonm/staging'
+}
