970230dda11f65613db76c2ec4f784152767cc2c
David Leon <david.leon@nexthink.com>
2021-08-27 12:08:40 +0200

    inherit class

--- src/com/davidleonm/WeatherStationDashboardVariables.groovy
+++ src/com/davidleonm/WeatherStationDashboardVariables.groovy
@@ -2,10 +2,10 @@ package com.davidleonm
 
 import com.davidleonm.GlobalVariables
 
-class WeatherStationDashboardVariables {
+class WeatherStationDashboardVariables extends GlobalVariables {
   // Github repository name
   static String RepositoryName = 'weather-station-project/dashboard'
-  
+
   // Registry name in Docker registry to push the image
   static String DockerRegistryName = 'weatherstationproject/dashboard'
 }
