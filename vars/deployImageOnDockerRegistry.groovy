eb6e0f95069921f1726ee3010554019d63865719
David Leon <david.leon@nexthink.com>
2021-08-30 17:35:38 +0200

    tabs

--- vars/deployImageOnDockerRegistry.groovy
+++ vars/deployImageOnDockerRegistry.groovy
@@ -1,4 +1,4 @@
-def call(String registryAddress, String registryName, String credentialsKey, String imageTag, String dockerFile, String, labelName, String, labelValue) {
+def call(String registryAddress, String registryName, String credentialsKey, String imageTag, String dockerFile, String labelName, String labelValue) {
   def dockerImage = null
 
   try {
@@ -9,17 +9,17 @@ def call(String registryAddress, String registryName, String credentialsKey, Str
           dockerImage.push("${imageTag}")
       }
   } finally {
-    if (dockerImage != null) {
-        if (labelName != null && labelValue != null) {
-            sh "docker image prune --filter label=${labelName}=${labelValue} --force"
-        }
+      if (dockerImage != null) {
+          if (labelName != null && labelValue != null) {
+              sh "docker image prune --filter label=${labelName}=${labelValue} --force"
+          }
 
-        def registryAddressWithoutHttp = "${registryAddress}".replace('http://', '')
-        sh """
-           docker image prune --filter label=${labelName}=${labelValue} --force
-           docker rmi -f ${registryAddressWithoutHttp}${registryName}:${imageTag}
-           docker rmi -f ${registryAddressWithoutHttp}${registryName}:latest
-           """
+            def registryAddressWithoutHttp = "${registryAddress}".replace('http://', '')
+            sh """
+               docker image prune --filter label=${labelName}=${labelValue} --force
+               docker rmi -f ${registryAddressWithoutHttp}${registryName}:${imageTag}
+               docker rmi -f ${registryAddressWithoutHttp}${registryName}:latest
+               """
+        }
     }
-  }
 }
