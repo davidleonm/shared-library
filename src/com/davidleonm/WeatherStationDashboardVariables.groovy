b4d125ac77c0619d3ec3d462074022b06f4fe89f
David Leon <david.leon@nexthink.com>
2021-09-01 22:33:10 +0200

    more variables

--- src/com/davidleonm/WeatherStationDashboardVariables.groovy
+++ src/com/davidleonm/WeatherStationDashboardVariables.groovy
@@ -6,6 +6,27 @@ class WeatherStationDashboardVariables {
   // Github repository name
   static String RepositoryName = 'weather-station-project/dashboard'
 
-  // Registry name in Docker registry to push the image
-  static String DockerRegistryName = 'weatherstationproject/dashboard'
+  // Registry name in Docker registry to push the image for the Dashboard
+  static String DashboardDockerRegistryName = 'weatherstationproject/dashboard'
+
+  // Registry name in Docker registry to push the image for the AirParametersService
+  static String AirParametersServiceDockerRegistryName = 'weatherstationproject/air-parameter-service'
+
+  // Registry name in Docker registry to push the image for the AmbientTemperatureService
+  static String AmbientTemperatureServiceDockerRegistryName = 'weatherstationproject/ambient-temperature-service'
+
+  // Registry name in Docker registry to push the image for the AuthenticationService
+  static String AuthenticationServiceDockerRegistryName = 'weatherstationproject/authentication-service'
+
+  // Registry name in Docker registry to push the image for the GatewayService
+  static String GatewayServiceDockerRegistryName = 'weatherstationproject/gateway-service'
+
+  // Registry name in Docker registry to push the image for the GroundTemperatureService
+  static String GroundTemperatureServiceDockerRegistryName = 'weatherstationproject/ground-temperature-service'
+
+  // Registry name in Docker registry to push the image for the RainfallService
+  static String RainfallServiceDockerRegistryName = 'weatherstationproject/rainfall-service'
+
+  // Registry name in Docker registry to push the image for the WindMeasurementsService
+  static String WindMeasurementsServiceDockerRegistryName = 'weatherstationproject/wind-measurements-service'
 }
