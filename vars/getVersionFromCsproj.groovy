66c02208186a60ea701a407714b9c1f88881fccc
David Leon <david.leon@nexthink.com>
2021-09-08 22:20:02 +0200

    no message

--- vars/getVersionFromCsproj.groovy
+++ vars/getVersionFromCsproj.groovy
@@ -1,3 +1,3 @@
 def call(String filePath) {
-    return sh(script: "grep '<Version>' < ${filePath} | sed 's/.*<Version>\(.*\)<\/Version>/\1/'", returnStdout: true)
+    return sh(script: "grep '<Version>' < ${filePath} | sed 's/.*<Version>\\(.*\\)<\\/Version>/\\1/'", returnStdout: true)
 }
