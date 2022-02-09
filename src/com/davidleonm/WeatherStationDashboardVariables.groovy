79add932f5be0518b4b5a11f5eac7adde5c4be89
David Leon <david.leon@nexthink.com>
2022-02-09 23:18:08 +0100

    no message

--- src/com/davidleonm/WeatherStationDashboardVariables.groovy
+++ src/com/davidleonm/WeatherStationDashboardVariables.groovy
@@ -3,30 +3,30 @@ package com.davidleonm
 import com.davidleonm.GlobalVariables
 
 class WeatherStationDashboardVariables {
-  // Github repository name
-  static String RepositoryName = 'weather-station-project/dashboard'
+    // Github repository name
+    static String RepositoryName = 'weather-station-project/dashboard'
 
-  // Registry name in Docker registry to push the image for the Dashboard
-  static String DashboardDockerRegistryName = 'weatherstationproject/dashboard'
+    // Registry name in Docker registry to push the image for the Dashboard
+    static String DashboardDockerRegistryName = 'weatherstationproject/dashboard'
 
-  // Registry name in Docker registry to push the image for the AirParametersService
-  static String AirParametersServiceDockerRegistryName = 'weatherstationproject/air-parameter-service'
+    // Registry name in Docker registry to push the image for the AirParametersService
+    static String AirParametersServiceDockerRegistryName = 'weatherstationproject/air-parameter-service'
 
-  // Registry name in Docker registry to push the image for the AmbientTemperatureService
-  static String AmbientTemperatureServiceDockerRegistryName = 'weatherstationproject/ambient-temperature-service'
+    // Registry name in Docker registry to push the image for the AmbientTemperatureService
+    static String AmbientTemperatureServiceDockerRegistryName = 'weatherstationproject/ambient-temperature-service'
 
-  // Registry name in Docker registry to push the image for the AuthenticationService
-  static String AuthenticationServiceDockerRegistryName = 'weatherstationproject/authentication-service'
+    // Registry name in Docker registry to push the image for the AuthenticationService
+    static String AuthenticationServiceDockerRegistryName = 'weatherstationproject/authentication-service'
 
-  // Registry name in Docker registry to push the image for the GatewayService
-  static String GatewayServiceDockerRegistryName = 'weatherstationproject/gateway-service'
+    // Registry name in Docker registry to push the image for the GatewayService
+    static String GatewayServiceDockerRegistryName = 'weatherstationproject/gateway-service'
 
-  // Registry name in Docker registry to push the image for the GroundTemperatureService
-  static String GroundTemperatureServiceDockerRegistryName = 'weatherstationproject/ground-temperature-service'
+    // Registry name in Docker registry to push the image for the GroundTemperatureService
+    static String GroundTemperatureServiceDockerRegistryName = 'weatherstationproject/ground-temperature-service'
 
-  // Registry name in Docker registry to push the image for the RainfallService
-  static String RainfallServiceDockerRegistryName = 'weatherstationproject/rainfall-service'
+    // Registry name in Docker registry to push the image for the RainfallService
+    static String RainfallServiceDockerRegistryName = 'weatherstationproject/rainfall-service'
 
-  // Registry name in Docker registry to push the image for the WindMeasurementsService
-  static String WindMeasurementsServiceDockerRegistryName = 'weatherstationproject/wind-measurements-service'
+    // Registry name in Docker registry to push the image for the WindMeasurementsService
+    static String WindMeasurementsServiceDockerRegistryName = 'weatherstationproject/wind-measurements-service'
 }
